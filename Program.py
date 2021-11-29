9b3ab002920731a91eab2ce96ed944e3465e648d
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2021-11-29 05:44:55 -0800

    * fixed some bugs @ time 5:44 AM FST Mon.
    * patched and created bugs :-)

--- Program.py
+++ Program.py
@@ -15,3 +15,62 @@ if varipa == "":
 else:
     print("ERROR 404 NO IPA FOUND:")
     sys.exit()
+
+    ## Sync a .apk file with android service and extract the contents to your /Documents folder
+
+    varAPK=input("Please your .apk file path: ")
+    if varAPK == "":
+        print("Please enter a valid path")
+        sys.exit()
+    else:
+        zipfile=varAPK+"_apk.zip.".extract.all()
+        print("Extracting contents to " + zipfile)
+        print("Sync to the Documents folder")
+        ## extract varAPK png files to documents folder
+        extractAPKPictuesToDocument=input("Please your .apk file path: ")
+        if extractAPKPictuesToDocument == "":
+            print("Please enter a valid path")
+            sys.exit()
+        else:
+            print("Extracting contents to " + extractAPKPictuesToDocument)
+            print("Sync to the Documents folder")
+            ## extract varAPK jpeg files to documents folder
+            extractAPKPictuesToDocument=input("Please your .apk file path: ")
+            if extractAPKPictuesToDocument == "":
+                print("Please enter a valid path")
+                sys.exit()
+            else:
+                print("Extracting contents to " + extractAPKPictuesToDocument)
+                print("Sync to the Documents folder")
+                ## extract varAPK jpeg files to documents folder
+                extractAPKPictuesToDocument=input("Please your .apk file path: ")
+                if extractAPKPictuesToDocument == "":
+                    print("Please enter a valid path")
+                    sys.exit()
+                else:
+                    print("Extracting contents to " + extractAPKPictuesToDocument)
+                    print("Sync to the Documents folder")
+                    ## extract varAPK jpeg files to documents folder
+                    extractAPKPictuesToDocument=input("Please your .apk file path: ")
+                    if extractAPKPictuesToDocument == "":
+                        print("Please enter a valid path")
+                        sys.exit()
+                    else:
+                        print("Extracting contents to " + input("Enter folder you would like to extract to "))
+                        print("Sync to the Documents folder")
+                        ## extract varAPK jpeg files to documents folder
+                        extractAPKPictuesToDocument=input("Please your .apk file path: ")
+                        if extractAPKPictuesToDocument == "":
+                            print("Please enter a valid path")
+                            sys.exit()
+                        else:
+                            print("Extracting contents to " + extractAPKPictuesToDocument)
+                            print("Sync to the Documents folder")
+                            ## extract varAPK jpeg files to documents folder
+                            extractAPKPictuesToDocument=input("Please your .apk file path: ")
+                            if extractAPKPictuesToDocument == "":
+                                print("Please enter a valid path")
+                                sys.exit()
+                            else:
+                                print("Done!")
+                                sys.exit()
